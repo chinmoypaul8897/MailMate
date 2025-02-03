1e732710e9fa522241c29109b57e0df87384f33f
chinmoy-paul <chinmoypaul8897@gmail.com>
2025-02-03 19:44:25 +0530

    project main file name change

--- src/main/java/com/email/email_writer/EmailWriterApplication.java
+++ src/main/java/com/email/email_writer/EmailWriterApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoProjectForSpringbootApplication {
+public class EmailWriterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoProjectForSpringbootApplication.class, args);
+		SpringApplication.run(EmailWriterApplication.class, args);
 	}
 
 }
