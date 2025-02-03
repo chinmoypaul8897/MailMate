56ebc086353ef688106ec1dabeeea45dca93d36e
chinmoy-paul <chinmoypaul8897@gmail.com>
2025-02-03 19:41:41 +0530

    email writer project created

---
+++ src/main/java/com/email/email_writer/DemoProjectForSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.email.email_writer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjectForSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjectForSpringbootApplication.class, args);
+	}
+
+}
