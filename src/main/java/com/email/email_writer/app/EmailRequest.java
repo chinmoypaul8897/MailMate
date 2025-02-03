3551ba29adcd8d89ade3c5b85d3a033df90bc70c
chinmoy-paul <chinmoypaul8897@gmail.com>
2025-02-03 20:42:35 +0530

    getters setters manually added lombok not working

--- src/main/java/com/email/email_writer/app/EmailRequest.java
+++ src/main/java/com/email/email_writer/app/EmailRequest.java
@@ -2,10 +2,32 @@ package com.email.email_writer.app;
 
 import lombok.Data;
 
-@Data
+//@Data
 public class EmailRequest {
     private String emailContent;
     private String tone ;
+    // getters and setters
+    public String getEmailContent() {
+        return emailContent;
+    }
 
+    public void setEmailContent(String emailContent) {
+        this.emailContent = emailContent;
+    }
 
+    public String getTone() {
+        return tone;
+    }
+
+    public void setTone(String tone) {
+        this.tone = tone;
+    }
+
+    @Override
+    public String toString() {
+        return "EmailRequest{" +
+                "emailContent='" + emailContent + '\'' +
+                ", tone='" + tone + '\'' +
+                '}';
+    }
 }
