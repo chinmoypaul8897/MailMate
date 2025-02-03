bb52373fbf839c1bea8681b32289922676460f68
chinmoy-paul <chinmoypaul8897@gmail.com>
2025-02-03 20:39:43 +0530

    EmailGeneratorService created

---
+++ src/main/java/com/email/email_writer/app/EmailGeneratorService.java
@@ -0,0 +1,50 @@
+package com.email.email_writer.app;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+
+@Service
+public class EmailGeneratorService {
+
+    @Value("${gemini.api.url}")
+    private String geminiApiUrl;
+
+    @Value("${gemini.api.key}")
+    private String geminiApiKey;
+
+
+    public String generateEmailReply(EmailRequest emailRequest)
+    {
+        // build the prompt
+        String prompt = buildPrompt(emailRequest);
+        // craft a request
+        Map<String,Object> requestBody = Map.of(
+                "contents",new Object[]{
+                        Map.of("parts",new Object[]{
+                                Map.of("text",prompt)
+                        })
+
+                }
+        );
+        // do request and get response
+
+
+        // return response
+
+
+    }
+
+    private String buildPrompt(EmailRequest emailRequest) {
+        StringBuilder prompt = new StringBuilder();
+        prompt.append("Generate a professional email reply for the following email content . Please dont generate a subject line  ");
+        if (emailRequest.getTone() != null && emailRequest.getTone().isEmpty())
+        {
+            prompt.append("Use a ").append(emailRequest.getTone()).append(" tone. ");
+        }
+        prompt.append("\nOriginal Email : \n").append(emailRequest.getEmailContent());
+        return prompt.toString();
+    }
+}
--- src/main/java/com/email/email_writer/app/EmailRequest.java
+++ src/main/java/com/email/email_writer/app/EmailRequest.java
@@ -6,4 +6,6 @@ import lombok.Data;
 public class EmailRequest {
     private String emailContent;
     private String tone ;
+
+
 }
